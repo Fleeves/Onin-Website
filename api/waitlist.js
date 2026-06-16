9dce756414e7ec86ca4d9ec14e6942acbba9a3a1
Fleeves <maxime.privatt@gmail.com>
2026-06-16 17:44:54 +0200

    Test Brevo auth

--- api/waitlist.js
+++ api/waitlist.js
@@ -1,5 +1,15 @@
 export default async function handler(req, res) {
-  return res.status(200).json({
-    hasKey: !!process.env.BREVO_API_KEY
-  });
-}
+
+    const response = await fetch(
+      'https://api.brevo.com/v3/account',
+      {
+        headers: {
+          'api-key': process.env.BREVO_API_KEY
+        }
+      }
+    );
+  
+    const data = await response.text();
+  
+    return res.status(response.status).send(data);
+  }
